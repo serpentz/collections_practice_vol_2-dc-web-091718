028161a883e87e13878fa58b0b540a8e23e5e47e
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-04 17:33:48 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,5 +1,5 @@
 # your code goes here
-def begins_with_r(collections)
+def begins_with_r(collection)
   collections.each do |word|
     if word[0] != 'r' then return false
     end
@@ -7,8 +7,12 @@ def begins_with_r(collections)
   true
 end
 
-def contain_a(collections)
+def contain_a(collection)
   new_col =  collections.select do |word|
      word.include?("a")
     end
   end
+
+def first_wa(collection)
+  collection.find{|x| x.include?("wa")}
+end
