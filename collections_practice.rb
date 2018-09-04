5d8acd44b40adae9633097a21c6894caf2d11f6a
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-04 17:22:43 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,12 +5,3 @@ def begins_with_r(collections)
     end
   end
 end
-
-def contain_a(collections)
-  new_col = []
-  collections.each do |word|
-    if word.include("a")  new_col << word
-    end
-  end
-
-end
