77828cfdf8ea62c8a41603cb4bad84808febb574
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-04 17:36:22 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,5 +14,5 @@ def contain_a(collection)
   end
 
 def first_wa(collection)
-  collection.find{|x|if(x.class == String.class)then x.include?("wa") end}
+  collection.find{|x|if x.class == String.class  x.include?("wa") else continue end}
 end
