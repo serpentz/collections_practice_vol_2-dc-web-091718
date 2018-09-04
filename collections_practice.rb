55f263a432d01715fbb3f44557c9f4c40c25ba50
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-04 17:20:21 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,7 +9,7 @@ end
 def contain_a(collections)
   new_col = []
   collections.each do |word|
-    if word.include?("a")   new_col.push(word)
+    if word.include?("a")  new_col<<word
     end
   end
   new_col
